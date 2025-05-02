dbb53226e2fd18021ab39e0b50d0ecb6635b5b55
v1shabh <tiwarivishabh@gmail.com>
2025-05-02 11:40:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load the model
+model = joblib.load(r"C:\Users\91830\OneDrive\Desktop\fd\fraud_detection_pipline.pkl")
+
+# App title
+st.title("Fraud Detection Prediction App")
+
+# App instructions
+st.markdown("Please enter the transaction details and use the Predict button.")
+
+st.divider()
+
+# Input fields
+transaction_type = st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER", "CASH_OUT", "DEPOSITE"])
+amount = st.number_input("Amount", min_value=0.0, value=1000.0)
+oldbalanceOrg = st.number_input("Old Balance (Sender)", min_value=0.0, value=10000.0)
+newbalanceOrig = st.number_input("New Balance (Sender)", min_value=0.0, value=9000.0)
+oldbalanceDest = st.number_input("Old Balance (Receiver)", min_value=0.0, value=0.0)
+newbalanceDest = st.number_input("New Balance (Receiver)", min_value=0.0, value=0.0)
+
+# Predict button
+if st.button("Predict"):
+    input_data = pd.DataFrame({
+        "type": [transaction_type],
+        "amount": [amount],
+        "oldbalanceOrg": [oldbalanceOrg],
+        "newbalanceOrig": [newbalanceOrig],
+        "oldbalanceDest": [oldbalanceDest],
+        "newbalanceDest": [newbalanceDest]
+    })
+
+    # Make prediction
+    prediction = model.predict(input_data)[0]
+
+    st.subheader(f"Prediction : '{int(prediction)}'")
+
+    if prediction ==1:
+        st.error("This transaction can be fraud")
+
+    else:
+        st.success("This transaction looks like it is not a fraud")
+
+
+
+        
+
+   
